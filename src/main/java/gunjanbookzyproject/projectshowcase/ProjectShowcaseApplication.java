d7dd64549aa28f33646b882a683dcfea2705ff43
student <k.gunjan88@gmail.com>
2022-07-03 13:06:22 +0000

    My SpringBoot Project

---
+++ src/main/java/gunjanbookzyproject/projectshowcase/ProjectShowcaseApplication.java
@@ -0,0 +1,13 @@
+package gunjanbookzyproject.projectshowcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectShowcaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectShowcaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gunjanbookzyproject/projectshowcase/ProjectShowcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package gunjanbookzyproject.projectshowcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectShowcaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
